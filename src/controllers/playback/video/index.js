60433b7ac5254e3a7696fcd28c51d16a1fcc7041
Mario Kakoulli <kakoulli@live.co.uk>
2024-05-13 21:44:27 +0100

    remove intro skip (injected from plugin now)

--- src/controllers/playback/video/index.js
+++ src/controllers/playback/video/index.js
@@ -520,34 +520,7 @@ export default function (view) {
             updatePlaylist();
             enableStopOnBack(true);
             updatePlaybackRate(player);
-            getIntroTimestamps(state.NowPlayingItem);
         }
-    }
-
-    function getIntroTimestamps(item) {
-        const apiClient = ServerConnections.getApiClient(item);
-        const address = apiClient.serverAddress();
-
-        const url = `${address}/Episode/${item.Id}/IntroTimestamps`;
-        const reqInit = {
-            headers: {
-                "Authorization": `MediaBrowser Token=${apiClient.accessToken()}`
-            }
-        };
-
-        fetch(url, reqInit).then(r => {
-            if (!r.ok) {
-                return;
-            }
-
-            return r.json();
-        }).then(intro => {
-            tvIntro = intro;
-        });
-    }
-
-    function skipIntro() {
-        playbackManager.seekMs(tvIntro.IntroEnd * 1000);
     }
 
     function onPlayPauseStateChanged() {
@@ -667,22 +640,6 @@ export default function (view) {
                 const item = currentItem;
                 refreshProgramInfoIfNeeded(player, item);
                 showComingUpNextIfNeeded(player, item, currentTime, currentRuntimeTicks);
-
-                // Check if an introduction sequence was detected for this item.
-                if (!tvIntro?.Valid) {
-                    return;
-                }
-
-                const seconds = playbackManager.currentTime(player) / 1000;
-                const skipIntro = document.querySelector(".skipIntro");
-
-                // If the skip prompt should be shown, show it.
-                if (seconds >= tvIntro.ShowSkipPromptAt && seconds < tvIntro.HideSkipPromptAt) {
-                    skipIntro.classList.remove("hide");
-                    return;
-                }
-
-                skipIntro.classList.add("hide");
             }
         }
     }
@@ -1594,7 +1551,6 @@ export default function (view) {
     let programEndDateMs = 0;
     let playbackStartTimeTicks = 0;
     let subtitleSyncOverlay;
-    let tvIntro;
     let trickplayResolution = null;
     const nowPlayingVolumeSlider = view.querySelector('.osdVolumeSlider');
     const nowPlayingVolumeSliderContainer = view.querySelector('.osdVolumeSliderContainer');
@@ -1751,10 +1707,6 @@ export default function (view) {
     let lastPointerDown = 0;
     /* eslint-disable-next-line compat/compat */
     dom.addEventListener(view, window.PointerEvent ? 'pointerdown' : 'click', function (e) {
-        // If the user clicked the skip intro button, don't pause the video. Fixes ConfusedPolarBear/intro-skipper#44.
-        if (dom.parentWithClass(e.target, ['btnSkipIntro'])) {
-            return;
-        }
         if (dom.parentWithClass(e.target, ['videoOsdBottom', 'upNextContainer'])) {
             showOsd();
             return;
@@ -1921,7 +1873,6 @@ export default function (view) {
     });
     view.querySelector('.btnAudio').addEventListener('click', showAudioTrackSelection);
     view.querySelector('.btnSubtitles').addEventListener('click', showSubtitleTrackSelection);
-    view.querySelector('.btnSkipIntro').addEventListener('click', skipIntro);
 
     // HACK: Remove `emby-button` from the rating button to make it look like the other buttons
     view.querySelector('.btnUserRating').classList.remove('emby-button');

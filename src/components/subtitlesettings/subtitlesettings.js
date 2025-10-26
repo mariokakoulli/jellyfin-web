3fcf0d3734c331fccbeafecbc12d0eb16d96bf4b
Mario Kakoulli <kakoulli@live.co.uk>
2025-10-26 21:45:17 +0000

    set subtitle setting (correctly)

--- src/components/subtitlesettings/subtitlesettings.js
+++ src/components/subtitlesettings/subtitlesettings.js
@@ -66,7 +66,7 @@ function loadForm(context, user, userSettings, appearanceSettings, apiClient) {
         context.querySelector('#sliderVerticalPosition').value = appearanceSettings.verticalPosition;
 
         context.querySelector('#selectSubtitleBurnIn').value = appSettings.get('subtitleburnin') || '';
-        context.querySelector('#chkSubtitleRenderPgs').checked = appSettings.get('subtitlerenderpgs') === 'true';
+        context.querySelector('#chkSubtitleRenderPgs').checked = 'true';
 
         context.querySelector('#selectSubtitleBurnIn').dispatchEvent(new CustomEvent('change', {}));
         context.querySelector('#chkAlwaysBurnInSubtitleWhenTranscoding').checked = appSettings.alwaysBurnInSubtitleWhenTranscoding();
